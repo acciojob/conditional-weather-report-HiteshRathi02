5905f726744a8ab5149042bd8c548c2b9bae2010
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-06-02 16:17:21 +0000

    update

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -9,7 +9,7 @@ export default function WeatherDisplay ({data}){
             <p>Temperature: {data.temperature}</p>
         </span>
         <p>
-            {data.conditions}
+            conditions: {data.conditions}
         </p>
         </>
     )
